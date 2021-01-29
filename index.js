d2c8260797a894b89a2ba84aae58ef9bd644bb40
unknown <danielpartida32@hotmail.com>
2021-01-29 16:33:10 +0100

    creada carpeta api dentro de routes con un archivo llamado carsRoutes

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express'); //Requiero express para poder trabajar con él
 const bodyParser = require('body-parser'); //Requiero body-parser para poder gestionar las peticiones post
 
+const apiRouter = require('./routes/api'); //Importo el fichero que gestiona las rutas
+
 const app = express(); //Creo la aplicación de express
 
 require('./db'); //Requiero el archivo de la base de datos
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api', apiRouter); //Le digo que todas las rutas que empiecen por api, las gestione el archivo api.js
 
 app.get('/', (req, res) => {
     res.send('Probando, probando... Sí, todo correcto!'); //Hago este get para comprobar que funciona el servidor
---
+++ routes/api.js
@@ -0,0 +1,5 @@
+const router = require('express').Router(); //Me traigo el router para gestionar mis rutas
+
+
+
+module.exports = router; //Y lo exporto
