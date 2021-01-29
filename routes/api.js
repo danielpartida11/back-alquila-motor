5f3c237cf149293a0105d7c431a23e2033164bcc
unknown <danielpartida32@hotmail.com>
2021-01-29 20:09:18 +0100

    añadidas las rutas de users al api.js

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 const router = require('express').Router(); //Me traigo el router para gestionar mis rutas
 
 const apiCarsRouter = require('./api/cars'); //Me traigo el archivo cars
+const apiUsersRouter = require('./api/users');
 
 router.use('/cars', apiCarsRouter);
+router.use('users', apiUsersRouter);
 
 module.exports = router; //Y lo exporto
