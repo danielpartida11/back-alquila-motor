6a1649aeb38f643ca7c96cade08a59c4d3d88e6a
unknown <danielpartida32@hotmail.com>
2021-01-29 22:24:54 +0100

    añadido el cors para no tener fallos

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express'); //Requiero express para poder trabajar con él
 const bodyParser = require('body-parser'); //Requiero body-parser para poder gestionar las peticiones post
+const cors = require('cors');
+const PORT = process.env.PORT || 3333 //Declaro la variable con el puerto al que me quiero conectar
 
 const apiRouter = require('./routes/api'); //Importo el fichero que gestiona las rutas
 
@@ -10,12 +12,21 @@ require('./db'); //Requiero el archivo de la base de datos
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', apiRouter); //Le digo que todas las rutas que empiecen por api, las gestione el archivo api.js
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('Probando, probando... Sí, todo correcto!'); //Hago este get para comprobar que funciona el servidor
 });
 
-app.listen(3333, () => {
+app.listen(PORT, () => {
     console.log('Servidor levantado en el puesto 3333!!'); //Función para arrancar el servidor en el puerto 3333
     //localhost:3333
 });
