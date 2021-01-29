c60b175c3b11a1db7b4a882649c6dfc4ec1e0471
unknown <danielpartida32@hotmail.com>
2021-01-29 14:02:08 +0100

    instalada la libreria mysql2 y hecha la conexion a la bases de datos

---
+++ db.js
@@ -0,0 +1,7 @@
+const Sequelize = require('sequelize'); //Me traigo la librería Sequelize para poder trabajar con ella
+
+const sequelize = new Sequelize('3726815_alquilamotor', '3726815_alquilamotor', '@f#O[.iF8G!mwia}', {
+    host: 'fdb30.atspace.me',
+    dialect: 'mysql'        //Con esta función, me conecto a mi base de datos con las siguientes creedenciales:
+                            //Nombre de la BBDD, usuario, contraseña, hosting y dialecto
+})
